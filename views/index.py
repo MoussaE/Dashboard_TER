a1b055834f58c489861a62f995c11f2b7e1022de
mohamed ramdane debiane <mohamed.debiane@ens.uvsq.fr>
2021-07-07 12:23:54 +0200

    Structure page index

--- views/index.py
+++ views/index.py
@@ -2,10 +2,11 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-import os 
+import dash_bootstrap_components as dbc
+import os
 import json
 import plotly.graph_objects as go
-import pandas as pd 
+import pandas as pd
 import plotly.express as px
 from data.fetcher import fetcher
 
@@ -18,17 +19,92 @@ df = pd.read_sql("select  t.table_name as Table,d.description, c.reltuples as Tu
   and  t.table_catalog = 'piratage' and t.table_name <> 'timestamps' ;", f.cnt.conn)
 
 
-index = html.Div([
-    html.H1(
-        children='Informations sur les table',
-        id='title-metadata',
-    ),
-    dash_table.DataTable(id="metadata",
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-    ),
-    html.H1(
-        children='Visualiastion des données',
-        id='visualisation',
-    ),
-])
+index = html.Div(
+    [
+        dbc.Row(dbc.Col(
+            dbc.Jumbotron([
+                dbc.Container(
+                    [
+                        html.H1(
+                            "Base de donnée sur le piratage de logiciels", className="display-3"),
+                        html.P(
+                            "",
+                            className="lead",
+                        ),
+                        html.P(
+                            "You will need to embed a fluid container in ",
+                            className="lead",
+                        ),
+                    ],
+                    fluid=True,
+                )
+            ],
+                fluid=True,
+            ))),
+        dbc.Row(dbc.Col(html.H1(
+            children='Informations sur les tables',
+            id='title-metadata',
+        ))),
+        dbc.Row(dbc.Col(dash_table.DataTable(id="metadata",
+                                             columns=[{"name": i, "id": i}
+                                                      for i in df.columns],
+                                             data=df.to_dict('records'),
+                                             ))),
+        dbc.Row(dbc.Col(html.H1(
+            children='Visualiastion des données',
+            id='visualisation',
+        ))),
+        dbc.Row(
+            [
+                dbc.Col(dbc.Card(
+                    [
+                        dbc.CardImg(src="/assets/map.png", top=True),
+                        dbc.CardBody(
+                            [
+                                html.H4("Cartes", className="card-title"),
+                                html.P(
+                                    "Visualisation des données cartographiques",
+                                    className="card-text",
+                                ),
+                                dbc.Button("Visualiser", color="primary"),
+                            ]
+                        ),
+                    ],
+                    style={"width": "18rem", "offset": "3"},
+                ), align="start"),
+                dbc.Col(html.Div(dbc.Card(
+                    [
+                        dbc.CardImg(src="/assets/map.png", top=True),
+                        dbc.CardBody(
+                            [
+                                html.H4("Graphiques", className="card-title"),
+                                html.P(
+                                    "Visualisation des données sous formes de graphiques",
+                                    className="card-text",
+                                ),
+                                dbc.Button("Visualiser", color="primary"),
+                            ]
+                        ),
+                    ],
+                    style={"width": "18rem"},
+                )), align="end"),
+                 dbc.Col(dbc.Card(
+                    [
+                        dbc.CardImg(src="/assets/map.png", top=True),
+                        dbc.CardBody(
+                            [
+                                html.H4("Comparatifs", className="card-title"),
+                                html.P(
+                                    "Comparaisons entre differentes valeurs",
+                                    className="card-text",
+                                ),
+                                dbc.Button("Visualiser", color="primary"),
+                            ]
+                        ),
+                    ],
+                    style={"width": "18rem", "offset": "3"},
+                ), align="center")
+            ]
+        ),
+    ]
+)
