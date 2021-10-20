95a916311a935a0a636785b3849850443f4158b4
Mohamed Ramdane Debiane <mohamed.debiane@ens.uvsq.fr>
2021-10-20 10:58:13 +0200

    Suppression des imports problématiques

--- views/footer.py
+++ views/footer.py
@@ -3,11 +3,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 import os 
 import json
-from dash_html_components.H2 import H2
-from dash_html_components.P import P
 import plotly.graph_objects as go
 import pandas as pd 
 import plotly.express as px
+
+
 #<a target="_blank" href="https://icones8.fr/icon/DoB2hWAWiTer/graphique-combin%C3%A9">Graphique Combiné</a> icône par <a target="_blank" href="https://icones8.fr">Icons8</a>
 
 footer = html.Footer(
